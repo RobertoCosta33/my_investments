08619635dc97f8ea2071345a014e8f660f85a56e
Roberto B. Costa <89531527+RobertoCosta33@users.noreply.github.com>
2025-10-19 07:03:21 -0300

    reinstalando venv

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Roberto\Documents\classroom\2025\pos-ia-preparacao\creating_modules_libs_in_pypi\env
+set VIRTUAL_ENV=C:\Users\Roberto\Documents\classroom\2025\pos-ia-preparacao\my_investments\env
 
 if not defined PROMPT set PROMPT=$P$G
 
